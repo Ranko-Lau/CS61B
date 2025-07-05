6e69bc3b03d59450b1acf333f417c3a63c735711
明月心 <rankolau@gmail.com>
2025-07-05 23:07:01 +0800

    lab05: Completed

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,29 +1,36 @@
 public class UnionFind {
     // TODO: Instance variables
+    private int size;
+    private int[] arr;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        size = N;
+        arr = new int[N];
+        for (int i = 0; i < N; i++){
+            arr[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return size;
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return arr[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
@@ -31,7 +38,19 @@ public class UnionFind {
        function, throw an IllegalArgumentException. */
     public int find(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        if (v > size - 1 || v < 0){
+            throw new IllegalArgumentException("Some comment to describe the reason for throwing.");
+        }
+        int p = v;
+        while (parent(p) >= 0) {
+            p = parent(p);
+        }
+        while (v != p){
+            int newp = parent(v);
+            arr[v] = p;
+            v = newp;
+        }
+        return p;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -41,6 +60,18 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if (root1 != root2){
+            if (-parent(root1) <= -parent(root2)){
+                arr[root2] += arr[root1];
+                arr[root1] = root2;
+            }
+            else{
+                arr[root1] += arr[root2];
+                arr[root2] = root1;
+            }
+        }
     }
 
 }
